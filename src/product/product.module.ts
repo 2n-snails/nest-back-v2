da35c225cb81a237bc98a23e36217a12cde1a00a
unknown <juren528@gmail.com>
2021-09-16 18:08:23 +0900

    feat(productUpload): create product upload function

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserModule } from 'src/user/user.module';
+import { ProductCreateService } from './query/productCreate.query.service';
 
 @Module({
-  providers: [ProductService],
+  imports: [AuthModule, UserModule],
+  providers: [ProductService, ProductCreateService],
   controllers: [ProductController],
 })
 export class ProductModule {}
