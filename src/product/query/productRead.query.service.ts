28957baa7ba69422f5b865f3227149ace94c43f9
unknown <juren528@gmail.com>
2021-09-16 23:53:58 +0900

    feat(product): product update and delete

--- src/product/query/productRead.query.service.ts
+++ src/product/query/productRead.query.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entity/product.entity';
+import { getRepository } from 'typeorm';
 
 @Injectable()
-export class ProductReadService {}
+export class ProductReadService {
+  async findSellerProduct(product_no: number) {
+    const seller = await getRepository(Product)
+      .createQueryBuilder('p')
+      .innerJoinAndSelect('p.user', 'u')
+      .select()
+      .where('p.product_no = :value', { value: product_no })
+      .getOne();
+    return seller;
+  }
+}
--- src/product/query/productUpdate.query.service.ts
+++ src/product/query/productUpdate.query.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Product } from 'src/entity/product.entity';
+import { State } from 'src/entity/state.entity';
+import { getRepository } from 'typeorm';
 
 @Injectable()
-export class ProductUpdateService {}
+export class ProductUpdateService {
+  async productUpdate(data: any, product_no: number) {
+    const { product_title, product_content, product_price } = data;
+    return await getRepository(Product)
+      .createQueryBuilder()
+      .update()
+      .set({
+        product_title,
+        product_content,
+        product_price,
+      })
+      .where('product_no = :value', { value: product_no })
+      .execute();
+  }
+
+  async productStateUpdate(product_no: number, state: string) {
+    return await getRepository(State)
+      .createQueryBuilder()
+      .update()
+      .set({ state })
+      .where('product = :value', { value: product_no })
+      .execute();
+  }
+}
