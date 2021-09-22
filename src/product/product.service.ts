bae4e11aced2783e220886c96edaef6e2f0b0444
hanjuren <juren528@gmail.com>
2021-09-22 12:42:14 +0900

    feat(product): productService Dto 추가

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -16,6 +16,9 @@ import { UpdateProductDto } from './dto/updateProduct.dto';
 import { ProductIdParamDto } from './dto/product.param.dto';
 import { CommentIdParamDto } from './dto/comment.param.dto';
 import { ReCommentIdParamDto } from './dto/recomment.param.dto';
+import { ChangeProductStateDto } from './dto/chageProduct.dto';
+import { CreateCommentDto } from './dto/createComment.dto';
+import { CreateReCommentDto } from './dto/createReComment.dto';
 
 @Injectable()
 export class ProductService {
@@ -26,11 +29,13 @@ export class ProductService {
     private readonly productDeleteService: ProductDeleteService,
     private readonly userService: UserService,
   ) {}
+  // 메인 페이지 상품 데이터 받기
   async getMainPageData(query: MainPageDto) {
     const data = await this.productReadService.findProducts(query);
     return data;
   }
 
+  // 상품 업로드 하기
   async createProduct(user_no: number, data: CreateProductDto) {
     const { product_title, product_content, product_price } = data;
     const product = await this.productCreateService.createProduct(
@@ -50,6 +55,7 @@ export class ProductService {
     return true;
   }
 
+  // 상품 수정하기
   async modifyProduct(
     user: User,
     data: UpdateProductDto,
@@ -74,6 +80,7 @@ export class ProductService {
     return true;
   }
 
+  // 상품 삭제하기
   async deleteProduct(user: User, product_id: ProductIdParamDto['product_id']) {
     const product = await this.productReadService.findSellerProduct(product_id);
     if (user.user_no !== product.user.user_no) {
@@ -88,10 +95,12 @@ export class ProductService {
     return true;
   }
 
+  // 상품 검색하기
   async searchProduct(query: SearchDto) {
     return await this.productReadService.search(query);
   }
 
+  // 상품 상세정보 받기
   async findOneProduct(product_id: ProductIdParamDto['product_id']) {
     const product = await this.productReadService.findOneProduct(product_id);
     // TODO: 댓글 목록 가져오기
@@ -102,9 +111,10 @@ export class ProductService {
     return { product, comment };
   }
 
+  // 상품 거래상태 수정하기
   async changeProductState(
     product_id: ProductIdParamDto['product_id'],
-    query: any,
+    query: ChangeProductStateDto,
   ) {
     const { state, user_no } = query;
     const user = await this.userService.findUserByUserNo(user_no);
@@ -118,30 +128,38 @@ export class ProductService {
       : { success: false, message: '상품 상태 수정 실패' };
   }
 
+  // 상품 판매자 찾기
   async findProductSeller(product_id: ProductIdParamDto['product_id']) {
     return await this.productReadService.findSellerProduct(product_id);
   }
 
+  // 찜 하기
   async createWish(
     product_id: ProductIdParamDto['product_id'],
-    user: number,
+    user: User['user_no'],
   ): Promise<Wish> {
     return await this.productCreateService.createWishData(product_id, user);
   }
 
-  async deletedWish(product_id: ProductIdParamDto['product_id'], user: number) {
+  // 짐 삭제하기
+  async deletedWish(
+    product_id: ProductIdParamDto['product_id'],
+    user: User['user_no'],
+  ) {
     return await this.productDeleteService.deleteWishData(product_id, user);
   }
 
+  // 상품 거래 상태 확인하기
   async checkProductState(
     product_id: ProductIdParamDto['product_id'],
   ): Promise<State> {
     return await this.productReadService.findProductStateData(product_id);
   }
 
+  // 찜 목록 확인하기
   async checkProductWishList(
     product_id: ProductIdParamDto['product_id'],
-    user_no: number,
+    user_no: User['user_no'],
   ): Promise<Wish> {
     return await this.productReadService.findProductWishListData(
       product_id,
@@ -149,9 +167,10 @@ export class ProductService {
     );
   }
 
+  // 댓글 업로드
   async createComment(
-    data: any,
-    user: number,
+    data: CreateCommentDto,
+    user: User['user_no'],
     product_id: ProductIdParamDto['product_id'],
   ): Promise<Comment> {
     return await this.productCreateService.createCommentData(
@@ -161,17 +180,20 @@ export class ProductService {
     );
   }
 
+  // 댓글 작성자 확인하기
   async checkCommentWriter(comment_no: CommentIdParamDto['comment_id']) {
     return await this.productReadService.findCommentWriterData(comment_no);
   }
 
+  // 댓글 삭제하기
   async deleteComment(comment_no: CommentIdParamDto['comment_id']) {
     return await this.productDeleteService.deleteCommentData(comment_no);
   }
 
+  // 대댓글 업로드
   async createReComment(
-    user_no: number,
-    data: any,
+    user_no: User['user_no'],
+    data: CreateReCommentDto,
     comment_no: CommentIdParamDto['comment_id'],
   ): Promise<ReComment> {
     return await this.productCreateService.createReCommentData(
@@ -181,12 +203,14 @@ export class ProductService {
     );
   }
 
+  // 대댓글 작성자 확인
   async checkReComment(
     recomment_no: ReCommentIdParamDto['recomment_id'],
   ): Promise<ReComment> {
     return await this.productReadService.findReCommentData(recomment_no);
   }
 
+  // 대댓글 삭제하기
   async deleteReComment(recomment_no: ReCommentIdParamDto['recomment_id']) {
     return await this.productDeleteService.deleteReCommentData(recomment_no);
   }
