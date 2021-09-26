58cb7a218694be739513566dc336cb0f223ec9ad
hanjuren <juren528@gmail.com>
2021-09-26 14:41:49 +0900

    fix(dto): product state change dto file fix

--- src/product/dto/chageProductState.dto.ts
+++ src/product/dto/chageProductState.dto.ts
@@ -3,13 +3,14 @@ import { IsIn, IsNotEmpty, IsNumberString, IsString } from 'class-validator';
 
 export class ChangeProductStateDto {
   @IsString()
-  @IsIn(['reservation', 'sold'], {
-    message: 'sold(판매완료), reservation(예약)',
+  @IsIn(['reservation', 'sold', 'sale'], {
+    message: 'sold(판매완료), reservation(예약), sale(판매중)',
   })
   @IsNotEmpty()
-  @ApiProperty({ enum: ['reservation', 'sold'] })
+  @ApiProperty({ enum: ['reservation', 'sold', 'sale'] })
   state: string;
 
+  @ApiProperty({ description: '예약한 유저 번호 또는 구매한 유저 번호' })
   @IsNumberString()
-  user_no: number;
+  user_no?: number;
 }
