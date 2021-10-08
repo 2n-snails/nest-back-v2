8c283ead7192c4b028a69fba53b5b5b13fe0c314
SY <12tndbs12@naver.com>
2021-10-08 16:07:50 +0900

    feat(): 소켓 연결

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -4,16 +4,17 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-@WebSocketGateway(4000, {
-  namespace: 'chat',
+@WebSocketGateway(8000, {
   cors: {
     origin: 'http://localhost:4000',
   },
+  transports: ['websocket'],
 })
 export class ChatGateway {
   @WebSocketServer()
-  server;
+  server: Server;
 
   @SubscribeMessage('message')
   handleEvent(@MessageBody() data: string): void {
