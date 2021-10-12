ddf00b26165c40c327953da407893e2aace84d71
SY <12tndbs12@naver.com>
2021-10-12 19:55:13 +0900

    feat(): chat 라우터 생성 및 완료

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -3,23 +3,43 @@ import { ChatService } from './chat.service';
 import {
   Controller,
   Get,
-  NotFoundException,
+  HttpException,
+  HttpStatus,
+  Param,
   Req,
   UseGuards,
 } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 
+@ApiTags('chat')
 @Controller('chat')
 export class ChatController {
   constructor(private chatService: ChatService) {}
 
+  @ApiOperation({
+    summary: '내가 가진 채팅목록',
+  })
   @UseGuards(JwtAccessAuthGuard)
   @Get('chatRoom')
   async findChatRoom(@Req() req: any) {
     const user = req.user;
 
     const result = await this.chatService.findAll(user.user_no);
-    if (!result) {
-      throw new NotFoundException('chat not found');
+    if (!result.length) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+    return result;
+  }
+
+  @ApiOperation({
+    summary: '채팅 내용',
+  })
+  @UseGuards(JwtAccessAuthGuard)
+  @Get('chatContent/:room_id')
+  async findChatContent(@Req() req: any, @Param('room_id') roomId: string) {
+    const result = await this.chatService.findOne(parseInt(roomId));
+    if (!result.length) {
+      throw new HttpException('NOT_FOUND', HttpStatus.NOT_FOUND);
     }
     return result;
   }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -4,13 +4,21 @@ import { getRepository } from 'typeorm';
 
 @Injectable()
 export class ChatService {
-  async findAll(id: number) {
+  async findAll(id: number): Promise<Chat[]> {
     const result = await getRepository(Chat)
       .createQueryBuilder('chat')
       .where(`chat.seller = :seller OR chat.buyer = :buyer`, {
         seller: id,
         buyer: id,
       })
+      .getRawMany();
+    return result;
+  }
+
+  async findOne(roomId: number): Promise<Chat[]> {
+    const result = await getRepository(Chat)
+      .createQueryBuilder('chat')
+      .where(`chat.chat_no = :chat_no`, { chat_no: roomId })
       .getRawMany();
     return result;
   }
